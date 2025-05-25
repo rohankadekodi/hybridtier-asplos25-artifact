eedbc1000c86c7f3648ab7779cd453e1bf891b6b
kevins981 <kevinsong981@gmail.com>
2025-05-24 21:55:02 -0400

    Remove unneeded files

--- hook/build.sh
+++
@@ -1,6 +0,0 @@
-#g++ -shared -fPIC hook.cpp -o hook.so -O3 -ldl -lpthread -lnuma
-#g++ -shared -fPIC hook.cpp -o hook.so -O3 -ldl -lpthread -lnuma -DTARGET_EXE_NAME=\"cachebench\"
-g++ -shared -fPIC hook.cpp -o hook.so -O3 -ldl -lpthread -lnuma -DTARGET_EXE_NAME=\"cachebench\" -DHYBRIDTIER_REGULAR
-#g++ -shared -fPIC hook.cpp -o hook.so -O3 -ldl -lpthread -lnuma -g -fno-omit-frame-pointer
-
-
