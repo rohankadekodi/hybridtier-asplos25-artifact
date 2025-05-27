eee054e41c8c139a307e6470f737c80fc2e7c660
kevins981 <kevinsong981@gmail.com>
2025-05-27 13:51:54 -0400

    Add scripts

---
+++ repro_hybridtier.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export BIGMEMBENCH_COMMON_PATH="/ssd1/songxin8/thesis/hybridtier/hybridtier-asplos25-artifact"
+
+echo "start" > ./exp_tracker
+
+# update boot up script
+cp ./rc.local.hybridtier /etc/rc.local
+
+# start with 32GB hybridtier kernel
+cp /etc/default/grubs_kevin/grub_hybridtier_32GB /etc/default/grub
+update-grub2
+reboot
+
