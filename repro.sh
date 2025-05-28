385f7aa53fd4b43aa8af038713032868adddcd70
kevins981 <kevinsong981@gmail.com>
2025-05-27 21:14:36 -0400

    add scripts

---
+++ repro.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export BIGMEMBENCH_COMMON_PATH="/ssd1/songxin8/thesis/hybridtier/hybridtier-asplos25-artifact"
+
+./repro_hybridtier.sh
+./repro_memtis.sh
---
+++ repro_memtis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export BIGMEMBENCH_COMMON_PATH="/ssd1/songxin8/thesis/hybridtier/hybridtier-asplos25-artifact"
+
+# update boot up script
+cp ./rc.local.memtis /etc/rc.local
+
+# memtis kernel
+cp /etc/default/grubs_kevin/grub_memtis /etc/default/grub
+update-grub2
+
+echo "Updated kernel. Rebooting in 10 seconds..."
+sleep 10
+
+reboot
+
