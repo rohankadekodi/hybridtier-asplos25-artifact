25b24d359ec47503a24712fc282aff005005664c
kevins981 <kevinsong981@gmail.com>
2025-05-30 17:23:00 -0400

    update path

--- repro.sh
+++ repro.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export BIGMEMBENCH_COMMON_PATH="/ssd1/songxin8/thesis/hybridtier/hybridtier-asplos25-artifact"
+export BIGMEMBENCH_COMMON_PATH="/ssd1/asplos25_ae/hybridtier-asplos25-artifact"
 
 ./repro_hybridtier.sh
 # memtis experiments will automatically be launched after hybrdtier experiments are done
